4f2e7723c098c573364e7580c00e8a116bf11a59
arjunyel <arjunyel@gmail.com>
2024-02-27 14:50:25 -0600

    chore(repo): Update Remix CF Pages playground to Vite

--- playground/remix-cf-pages/app/root.tsx
+++ playground/remix-cf-pages/app/root.tsx
@@ -1,35 +1,33 @@
-import type { LoaderFunction, MetaFunction } from '@remix-run/cloudflare';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 import { rootAuthLoader } from '@clerk/remix/ssr.server';
-import { ClerkApp, ClerkErrorBoundary } from '@clerk/remix';
+import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
+import { ClerkApp } from '@clerk/remix';
 
-export const loader: LoaderFunction = args => {
-  return rootAuthLoader(args);
-};
+export const loader = (args: LoaderFunctionArgs) => rootAuthLoader(args);
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
-});
-
-function App() {
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <head>
+        <meta charSet='utf-8' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
 }
 
-export default ClerkApp(App);
+function App() {
+  return <Outlet />;
+}
 
-export const ErrorBoundary = ClerkErrorBoundary();
+export default ClerkApp(App);
